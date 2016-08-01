72d48184709a82a99f57b407aabf0c650b4e300a
Adán G. Galván González <bialikoer@gmail.com>
2016-08-01 12:19:04 -0400

    repaint dom

--- script.js
+++ script.js
@@ -1,38 +1,47 @@
+let childs = document.getElementById("slots").innerHTML;
 var lever = document.getElementById('lever');
 lever.addEventListener('click', pullReel);
 
 let options = {
-  reel1:['coffe-machine', 'teapot', 'espresso-machine'],
-  reel2:['coffe-filter', 'tea-strainer', 'espresso-tamper'],
-  reel3:['coffe-grounds', 'tea-bag', 'espresso-beans'],
+  reel1:['coffee-machine', 'teapot', 'espresso-machine'],
+  reel2:['coffee-filter', 'tea-strainer', 'espresso-tamper'],
+  reel3:['coffee-grounds', 'tea-bag', 'espresso-beans'],
   prize: ['coffee-cup', 'tea-cup', 'espresso-cup']
 };
 
 function pullReel(e) {
     if(!e.currentTarget.checked){
       let luck = randomize();
+      renderLuck(luck);
       var winner = isWinner(luck);
       console.log(winner);
-      //stopAnimation();
-      //renderLuck();
       if (winner) {
         alert('Cool! you win a ' + options.prize[luck.reel1.index]);
       } else {
         alert('Sorry, Try again!');
       }
     }else{
-     // reset();
-      //startAnimation();
+      reset();
     }
+}
+
+function reset(){
+  document.getElementById("slots").innerHTML = childs;
+}
+
+function renderLuck(luck){
+  let html = '';
+  html+='<div class="board item'+ luck.reel1.index +'"><div class="reel"><span class="thmbtck-cb'+luck.reel1.display+'"></span></div></div>';
+  html+='<div class="board item'+ luck.reel2.index +'"><div class="reel"><span class="thmbtck-cb'+luck.reel2.display+'"></span></div></div>';
+  html+='<div class="board item'+ luck.reel3.index +'"><div class="reel"><span class="thmbtck-cb'+luck.reel3.display+'"></span></div></div>';
+  document.getElementById("slots").innerHTML = html;
 }
 
 function randomize() {
   let idx1 = Math.floor(Math.random() * (2));
   let idx2 = Math.floor(Math.random() * (2));
   let idx3 = Math.floor(Math.random() * (2));
-  console.log(options.reel1[idx1]);
-  console.log(options.reel2[idx2]);
-  console.log(options.reel3[idx3]);
+  console.log(options.reel1[idx1], options.reel2[idx2], options.reel3[idx3]);
   return {
     reel1: { display: options.reel1[idx1], index: idx1 },
     reel2: { display: options.reel2[idx2], index: idx2 },
