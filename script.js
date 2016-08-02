3d93f35e29fdc1916738842c578a64196bd8cfa5
Adán G. Galván González <bialikoer@gmail.com>
2016-08-02 00:14:02 -0400

    add keyboard nav and modal

--- script.js
+++ script.js
@@ -1,32 +1,82 @@
 let childs = document.getElementById("slots").innerHTML;
 let lever = document.getElementById('lever');
 lever.addEventListener('click', pullReel);
+document.body.addEventListener('keydown', checkKey);
+
+let modal = document.getElementById('modal');
+modal.addEventListener('click', dismiss);
 
 let options = {
   reel1:['coffee-machine', 'teapot', 'espresso-machine'],
   reel2:['coffee-filter', 'tea-strainer', 'espresso-tamper'],
   reel3:['coffee-grounds', 'tea-bag', 'espresso-beans'],
-  prize: ['coffee-cup', 'tea-cup', 'espresso-cup']
+  prize: [{
+    name: 'cup of coffee',
+    icon: '<span class="thmbtck-cbcoffee-cup"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span></span>'
+  }, {
+    name: 'cup of tea',
+    icon: '<span class="thmbtck-cbtea-cup"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span><span class="path9"></span></span>'
+  }, {
+    name: 'cup of espresso',
+    icon: '<span class="thmbtck-cbespresso-cup"><span class="path1"></span><span class="path2"></span><span class="path3"></span><span class="path4"></span><span class="path5"></span><span class="path6"></span><span class="path7"></span><span class="path8"></span></span>'
+  }]
 };
 
-function pullReel(e) {
-    if(!e.currentTarget.checked){
+function checkKey(e){
+    if(e.which === 76){
+      console.log("triggered L key");
+      lever.checked = !lever.checked;
+      pullReel();
+    }
+    if(e.which == 27){
+      dismiss();
+    }
+}
+
+function pullReel() {
+    if(!lever.checked ){
       let luck = randomize();
       renderLuck(luck);
       var winner = isWinner(luck);
-      console.log(winner);
+      var message = '<small>'+luck.reel1.display+', '+luck.reel2.display+' and ' +luck.reel3.display+'... </small>';
       if (winner) {
-        alert('Cool! you win a ' + options.prize[luck.reel1.index]);
+          message += buildPrize(options.prize[luck.reel1.index]);
       } else {
-        alert('Sorry, Try again!');
+          message += buildLose();
       }
+      document.getElementById('message').innerHTML = message;
+      display(winner);
     }else{
       reset();
+      dismiss();
     }
+}
+
+function display(win){
+  modal.classList.remove('win');
+  modal.classList.remove('lose');
+  win ? modal.classList.add('win'): modal.classList.add('lose');
+  modal.style.display = 'block';
+  modal.classList.remove('bounceOut');
+  modal.classList.add('bounceIn');
+}
+
+function dismiss(e){
+  modal.classList.remove('bounceIn');
+  modal.classList.add('bounceOut');
 }
 
 function reset(){
   document.getElementById("slots").innerHTML = childs;
+}
+
+function buildPrize(prize){
+  return  'Awesome! you win a '+'<b>'+ prize.name + '</b> '+prize.icon + ' mmm... Enjoy!!!';
+}
+
+function buildLose(){
+  return 'Nothing now, but try agin'; 
+  
 }
 
 function renderLuck(luck){
